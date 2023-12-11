8cd94fcab5a4e9ac837ce72bcef52a9c469a752a
VrajDarji <vrajdarji573@gmail.com>
2023-12-11 16:13:16 +0530

    intial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,80 @@
-import Image from 'next/image'
+"use client";
+
+import { Pause, Play } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [time, setTime] = useState("00:00:00");
+  const [inputMinutes, setInputMinutes] = useState(0);
+  const [inputSeconds, setInputSeconds] = useState(inputMinutes * 60);
+  const [isStarted, setIsStarted] = useState(false);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const formatTime = (data: number) => {
+    const hours = Math.floor(data / 3600);
+    const minutes = Math.floor((data % 3600) / 60);
+    const second = Math.floor(data % 60);
+    setTime(
+      `${hours < 10 ? "0" : ""}${hours}:${minutes < 10 ? "0" : ""}${minutes}:${
+        second < 10 ? "0" : ""
+      }${second}`
+    );
+    console.log({ hours, minutes, second });
+  };
+
+  useEffect(() => {
+    setInputSeconds(inputMinutes * 60);
+    formatTime(inputMinutes * 60);
+  }, [inputMinutes]);
+
+  useEffect(() => {
+    let countdown: NodeJS.Timeout;
+    if (inputSeconds > 0 && isStarted) {
+      countdown = setInterval(() => {
+        setInputSeconds((prev) => prev - 1);
+        formatTime(inputSeconds - 1);
+      }, 1000);
+    }
+    if (inputSeconds === 0) {
+      setIsStarted(false);
+    }
+    return () => clearInterval(countdown);
+  }, [inputSeconds, isStarted, inputMinutes]);
+  return (
+    <div className="h-[100vh] w-full flex justify-center items-center flex-col gap-4 bg-[#1C232F] text-[#06ACCD]">
+      <div className="flex flex-col">
+        <label htmlFor="time" className="cursor-pointer text-sm mb-1">
+          Enter Minutes
+        </label>
+        <input
+          type="text"
+          id="time"
+          className="px-2 py-1 outline-none rounded-md w-[20rem] text-black"
+          onChange={(e) => {
+            setInputMinutes(+e.target.value);
+          }}
+          value={inputMinutes}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="flex flex-row gap-3 justify-center items-center">
+        <button
+          onClick={() => setIsStarted((prev) => !prev)}
+          className="flex h-14 w-14 rounded-full justify-center items-center bg-[#06accd] text-[#1c232f] text-xl"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          {isStarted ? <Pause fill="#1c232f" /> : <Play fill="#1c232f" />}
+        </button>
+        <p className="text-[3.5rem] font-semibold">{time}</p>
       </div>
-    </main>
-  )
+      <button
+        onClick={() => {
+          setInputMinutes(0);
+          setInputSeconds(0);
+          formatTime(0);
+          setIsStarted(false);
+        }}
+        className="bg-[#06accd] px-3 py-2 rounded-lg border border-[#06accd] text-[#1c232f] font-semibold text-base hover:bg-[#1c232f] hover:text-[#06accd]"
+      >
+        Reset
+      </button>
+    </div>
+  );
 }
